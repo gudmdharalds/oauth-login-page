2d6d7e9e7ae4fb1c38c0d88c786f0a47d0abd0ce
Gudmundur D. Haraldsson <gdh1@hi.is>
2015-11-24 11:36:30 +0000

    Added templates.

---
+++ tpl/error.tpl.php
@@ -0,0 +1,9 @@
+<h1>There has been a fatal error</h1>
+
+<p>While processing your request we encountered a fatal error:</p>
+
+<p>%error_msg%</p>
+
+<p>This error is very probably a temporary one. Please excuse the inconvenience and try again in a short while.</p>
+
+
--- tpl/login-form.tpl.php
+++ tpl/login-form.tpl.php
@@ -1,9 +1,9 @@
 <form action="/" method="POST">
 
-<h1>:h1_caption</h1>
+<h1>%h1_caption%</h1>
 
 
-<img src=":image_page">
+<img src="%image_page%">
 
 <p>
 Username:	<input type="text" name="username" value="">
@@ -13,8 +13,8 @@ Username:	<input type="text" name="username" value="">
 Password:	<input type="text" name="password" value="">
 </p>
 
-<input type="hidden" name="redirect_uri" value=":redirect_uri">
-<input type="hidden" name="nonce" value=":nonce">
+<input type="hidden" name="redirect_uri" value="%redirect_uri%">
+<input type="hidden" name="nonce" value="%nonce%">
 
 <input type="submit">
 
