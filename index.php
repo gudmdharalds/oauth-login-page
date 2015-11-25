9ab5ae035910d9ab0038ff2d50a255965771c3fb
Gudmundur D. Haraldsson <gdh1@hi.is>
2015-11-25 15:15:50 +0000

    Now handling 401 errors more gracefully, actually presenting users with the error message received from the OAuth server.

--- index.php
+++ index.php
@@ -50,6 +50,9 @@ function lp_init_check() {
 			"image_icon",
 			"page_title_prefix",
 			"css_file",
+			"login_form_heading",
+			"login_form_error_prefix",
+			"login_form_error_suffix",
 			"nonce_secret_key",
 			"oauth2_server_access_token_uri",
 			"oauth2_client_id",
@@ -263,14 +266,20 @@ else if (
 
 		$oauth_req_response_json = curl_exec($oauth_req_curl_handle);
 
+
 		// Check if the request succeeded, if not, return an error to user.
 		if ($oauth_req_response_json === FALSE) {
 			lp_login_form("Unable to authenticate for some reason.");
 		}
-		
+
+
+		// Get HTTP status code of our request
 		$oauth_req_http_headers = curl_getinfo($oauth_req_curl_handle);
 
-		if ($oauth_req_http_headers["http_code"] === 200) {
+		if (
+			($oauth_req_http_headers["http_code"] === 200) ||
+			($oauth_req_http_headers["http_code"] === 401)
+		) {
 			$oauth_req_response_arr = json_decode($oauth_req_response_json);
 
 			// If decoding of JSON does not succeed, we cannot do anything. */
@@ -278,41 +287,53 @@ else if (
 				lp_fatal_error("Could not decode response from OAuth server");
 			}
 
+			$oauth_req_response_arr = (array) $oauth_req_response_arr;
+		}
+
+
+		/*
+		 * Now handle each HTTP status code separately
+		 */		
+
+		if ($oauth_req_http_headers["http_code"] === 200) {
+			/*
+			 * OK, decoding seems to have succeeded,
+			 * start by converting the result into an array.
+			 */
+
+			$oauth_req_response_arr = (array) $oauth_req_response_arr;
+
+			/*
+			 * Now check if we actually got an access_token 
+			 * to use. If so, redirect the browser to the redirect_uri specified
+			 * in the request. 
+			 * 
+			 * Remember: This URI has already been verified to be registered
+			 * with us, at the beginning of processing of this request.
+			 */
+			
+			if (isset($oauth_req_response_arr["access_token"])) {
+				header('HTTP/1.1 302 Found');
+				header('Location: ' . urldecode($_REQUEST{"redirect_uri"}) . 
+					'?access_token=' . $oauth_req_response_arr["access_token"]);
+			}
+			
 			else {
 				/*
-				 * OK, decoding seems to have succeeded,
-				 * start by converting the result into an array.
+				 * We did not get an access token, so we cannot continue. 
+				 * Show user a login form.
 				 */
 
-				$oauth_req_response_arr = (array) $oauth_req_response_arr;
-
-				/*
-				 * Now check if we actually got an access_token 
-				 * to use. If so, redirect the browser to the redirect_uri specified
-				 * in the request. 
-				 * 
-				 * Remember: This URI has already been verified to be registered
-				 * with us, at the beginning of processing of this request.
-				 */
-				
-				if (isset($oauth_req_response_arr["access_token"])) {
-					header('HTTP/1.1 302 Found');
-					header('Location: ' . urldecode($_REQUEST{"redirect_uri"}) . '?access_token=' . $oauth_req_response_arr["access_token"]);
-				}
-			
-				else {
-					/*
-					 * We did not get an access token, so we cannot continue. 
-					 * Show user a login form.
-					 */
-
-					lp_login_form("Authentication failure.");
-				}
+				lp_login_form("Authentication failure.");
 			}
 		}
 
 		else if ($oauth_req_http_headers["http_code"] === 401) {
-			lp_login_form("Wrong username or password?");
+			lp_login_form(
+				(empty($oauth_req_response_arr["message"]) !== TRUE) ?
+				$oauth_req_response_arr["message"] :
+				"Wrong username or password?"
+			);
 		}
 
 		else if ($oauth_req_http_headers["http_code"] === 500) {
