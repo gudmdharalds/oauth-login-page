8be502086faecc4d5f9d729a92e56fddbfdb7bdd
Gudmundur D. Haraldsson <gdh1@hi.is>
2015-11-27 12:45:30 +0000

    Added more tests.

--- tests/nonceTest.php
+++ tests/nonceTest.php
@@ -3,50 +3,76 @@
 require_once(__DIR__ . "/../nonce.php");
 
 class Nonce extends PHPUnit_Framework_TestCase {
-	
+	public function __construct() {
+		global $lp_config;
+
+		$lp_config["nonce_hashing_function"] = "sha256";
+
+		$this->static_secret = (string) rand() . "RanDomNssNotReally";
+		$this->session_secret = (string) rand() . "SomeOtherRAndomNess";
+	}
+
+	public function __destruct() {
+		global $lp_config;
+
+		unset($this->static_secret);
+		unset($this->session_secret);
+
+		unset($lp_config["nonce_hashing_function"]);
+	}
 
 	public function test_generate_static_secret_empty() {
-    
+		$nonce = @lp_nonce_generate(
+			"",
+			$this->session_secret,
+			100
+		);
+
+		$this->assertFalse($nonce);
 	}
+
     
 	public function test_generate_session_secret_empty() {
-    
+ 		$nonce = @lp_nonce_generate(
+			$this->static_secret,
+			"",
+			100
+		);
+
+		$this->assertFalse($nonce);
 	}
-    
+   
+ 
 	public function test_generate_timeout_invalid() {
-    
+ 		$nonce = @lp_nonce_generate(
+			$this->static_secret,
+			$this->session_secret,
+			-50
+		);
+
+		$this->assertFalse($nonce);
 	}
-    
+   
+ 
 	public function test_generate_hash_function_invalid() {
     		global $lp_config;
 
 		$lp_config["nonce_hashing_function"] = "smurningur100";
 	
-		$static_secret = (string) rand() . "RanDomNssNotReally";
-		$session_secret = (string) rand() . "SomeOtherRAndomNess";
-
-		$nonce = lp_nonce_generate(
-			$static_secret,
-			$session_secret,
+		$nonce = @lp_nonce_generate(
+			$this->static_secret,
+			$this->session_secret,
 			100
 		);
-	
 
-		$self->assertFalse($nonce);
-	
+		$this->assertFalse($nonce);
 	}
+
     
 	public function test_generate_ok() {
-		global $lp_config;
-	
-		$lp_config["nonce_hashing_function"] = "sha256";
-	
-		$static_secret = (string) rand() . "RanDomNssNotReally";
-		$session_secret = (string) rand() . "SomeOtherRAndomNess";
-
-			$nonce = lp_nonce_generate(
-			$static_secret,
-			$session_secret,
+		$nonce = lp_nonce_generate(
+			$this->static_secret,
+			$this->session_secret,
 			100
 		);
 	
@@ -77,10 +103,102 @@ class Nonce extends PHPUnit_Framework_TestCase {
 		// Check if the nonce checks out good
 		$this->assertTrue(
 			lp_nonce_check(
-				$static_secret,
-				$session_secret,
+				$this->static_secret,
+				$this->session_secret,
 				$nonce
 			)
+		);
+	}
+
+
+	public function test_check_nonce_not_string() {
+		$this->assertFalse(
+			@lp_nonce_check(
+					$this->static_secret,
+					$this->session_secret,
+					1993
+			)
+		);
+
+		$this->assertEquals(
+			error_get_last()["message"],
+			"Invalid nonce - not string"
+		);
+	}
+
+
+	public function test_check_nonce_invalid_length() {
+		$this->assertFalse(
+			@lp_nonce_check(
+					$this->static_secret,
+					$this->session_secret,
+					"a,b"
+			)
+		);
+
+		$this->assertEquals(
+			error_get_last()["message"],
+			"Invalid nonce - illegal size"
+		);
+
+
+		$this->assertFalse(
+			@lp_nonce_check(
+					$this->static_secret,
+					$this->session_secret,
+					"a,b,c,d"
+			)
+		);
+
+		$this->assertEquals(
+			error_get_last()["message"],
+			"Invalid nonce - illegal size"
+		);
+	}
+
+
+	public function test_check_nonce_expired() {
+		$this->assertFalse(
+			@lp_nonce_check(
+					"somemegasecretstring1",
+					"othermegasecretstring2",
+					"CaynL1OJvzksHzfFFi0Sny4JaN8NDEQLMa0OIcYEJdR8eR6//k0cHuYWEoGJQYQs/WKHIPKWgd3mli4r,1448627505,4037e5b7cca1fa455fac887bf4ddbdfa844b4ba31b6749152b09822d383ac7af"
+			)
+		);
+
+		$this->assertEquals(
+			error_get_last()["message"],
+			"Nonce has expired"
+		);
+	}
+
+
+	public function test_check_nonce_ok() {
+		$static_secret = "someULTRAmegasecretstring1";
+		$session_secret = "someULTRAmegaSECRETstring2";
+
+		$nonce1 = lp_nonce_generate(
+			$static_secret,
+			$session_secret,
+			5
+		);
+
+		$this->assertFalse(
+			@lp_nonce_check(
+				$static_secret,
+				$session_secret,
+				$nonce1
+			) === FALSE	
+		);
+	
+		sleep(6);
+	
+		$this->assertTrue(
+			@lp_nonce_check(
+				$static_secret,
+				$session_secret,
+				$nonce1
+			) === FALSE	
 		);
 	}
 
