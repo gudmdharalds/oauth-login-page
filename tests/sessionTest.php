f012f554796ee2052dbd7d11f185a7852e47a537
Gudmundur D. Haraldsson <gdh1@hi.is>
2015-12-01 14:40:02 +0000

    Now testing lp_session_init() sufficently well enough.

--- tests/sessionTest.php
+++ tests/sessionTest.php
@@ -11,7 +11,11 @@ function __lp_unittesting_lp_fatal_error($error_msg) {
 }
 
 function __lp_unittesting_session_start() {
+	global $_SESSION;
+
+	return TRUE;
 }
+
 
 class SessionTest extends \PHPUnit_Framework_TestCase {
 	public function __construct() {
@@ -46,15 +50,30 @@ class SessionTest extends \PHPUnit_Framework_TestCase {
 		);	
 	}
 
+
 	public function test_session_init_ok() {
 		global $lp_config;
+		global $_SERVER;
+		global $_SESSION;
 
-		
+		/*
+		 * Use our own session_start() function (see above).
+		 * Note that a session will not be really started,
+		 * but we can nevertheless test what $_SESSION contains.
+		 *
+		 *
+		 * Specify our own hashing function and entropy length -
+		 * will be set with ini_set().
+	 	 */
 
+		$lp_config["session_start_func"] = '__lp_unittesting_session_start';
 		$lp_config["session_hashing_function"] = "sha1";
-		$lp_config["session_entropy_length"] = "sha1";
+		$lp_config["session_entropy_length"] = 136;
 
 		ini_set('track_errors', 1);
+
+		$_SERVER['HTTP_USER_AGENT'] = 'Mozilla/1.0';
+
 
 		try { 	
 			lp_session_init();
@@ -62,8 +81,29 @@ class SessionTest extends \PHPUnit_Framework_TestCase {
 
 		catch (Exception $e) {
 			$fatal_error = $e->getMessage();
+
+			$this->assertEquals($fatal_error, "");
 		}
 
+		/*
+	 	 * The session secret generated
+		 * and then validate it ...
+		 */
+		
+		$lp_nonce_session_secret = $_SESSION["lp_nonce_session_secret"];
+
+		$this->assertTrue(
+			is_string($lp_nonce_session_secret)
+		);
+
+		$this->assertTrue(
+			strlen($lp_nonce_session_secret) > 0
+		);
+
+
+		/* 
+		 * Check if all config variables were set
+		 */
 
 		$this->assertEquals(
 			ini_get('session.use_trans_sid'),
@@ -76,15 +116,15 @@ class SessionTest extends \PHPUnit_Framework_TestCase {
 		);
 
 		/*
-		 * Welcome to PHP: This test always fails.
+		 * This setting is only supported in versions 5.5.2 or newer.
 		 */
-
-		/*
-		$this->assertEquals(
-			ini_get('session.use_strict_mode'),
-			1
-		);
-		*/
+		        
+		if (version_compare(PHP_VERSION, '5.5.2', '>=') === TRUE) {
+			$this->assertEquals(
+				ini_get('session.use_strict_mode'),
+				1
+			);
+		}
 
 		$this->assertEquals(
 			ini_get('session.cookie_httponly'),
@@ -100,7 +140,7 @@ class SessionTest extends \PHPUnit_Framework_TestCase {
 
 		$this->assertEquals(
 			ini_get("session.entropy_length"), 
-			"sha1"
+			136
 		);
 
 
@@ -110,8 +150,45 @@ class SessionTest extends \PHPUnit_Framework_TestCase {
 		);
 
 
-		// And here testing of this function ends.
-		// PHP unit-testing and sessions don't mix well.
+		/*
+		 * Fake another request - all should
+		 * be fine, as the 'User-agent' is the same ...
+		 */
+
+		try {
+			lp_session_init();
+		}
+
+		catch (Exception $e) {
+			$this->assertEqual("", $e->getMessage());
+		}
+
+		$this->assertEquals($lp_nonce_session_secret, $_SESSION["lp_nonce_session_secret"]);
+
+
+		/* 
+		 * Fake yet another request, but with
+		 * a different user-agent string - now
+	 	 * things should hit the fan.
+		 */
+
+		$_SERVER["HTTP_USER_AGENT"] = "Internet Explorer 3.0";
+
+		try {
+			lp_session_init();
+		}
+
+		catch (Exception $e) {
+			/*
+			 * This should happen when setcookie() is called
+			 * by lp_session_init() -- and so we assume things
+			 * are just fine.
+			 */
+
+			$this->assertContains("Cannot modify header information - headers already sent by", $e->getMessage());
+		}
+	
+		$this->assertEquals($lp_nonce_session_secret, $_SESSION["lp_nonce_session_secret"]);	
 	}
 
 	public function test_session_handler_open_ok() {
