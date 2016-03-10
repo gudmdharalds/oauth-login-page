7d7a674c2c005965c772297db9b290c1dbf2f195
Gudmundur D. Haraldsson <gdh1@hi.is>
2016-03-10 21:13:04 +0000

    Actually including filters into core. Now loads up available customizations.

--- includes.php
+++ includes.php
@@ -15,5 +15,32 @@ require_once(__DIR__ . "/misc.php");
 require_once(__DIR__ . "/html.php");
 require_once(__DIR__ . "/session.php");
 require_once(__DIR__ . "/nonce.php");
+require_once(__DIR__ . "/filters.php");
+
+
+/*
+ * Now load any .php files that
+ * might have been put in "customizations"
+ * folder.
+ */
+
+$customizations_dir_path = __DIR__ . "/customizations/";
+$customizations_files = scandir($customizations_dir_path);
+
+foreach ($customizations_files as $customizations_file_item) {
+	$tmp = explode(".php", $customizations_file_item);
+
+	if ((count($tmp) !== 2) && ($tmp[1] !== "")) {
+		continue;
+	}
+
+	if (file_exists($customizations_dir_path . "/oauth2_calls.php")) {
+		require_once($customizations_dir_path . "/oauth2_calls.php");
+	}
+}
+
+unset($customizations_dir_path);
+unset($customizations_files);
+unset($tmp);
 
 
